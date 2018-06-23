da1d33dda9a6e2e93e1722c05a62fceaadbefa49
Suo Tan <tandysony@gmail.com>
2018-06-22 23:09:36 -0400

    Rewrite the app using Vue.js

---
+++ Apps/Migrate-App-To-VueJS/invoice-vuejs/static/js/main.js
@@ -0,0 +1,46 @@
+const app = new Vue({
+  el: "table",
+
+  data: {
+    items: [
+      { description: "Website design", quantity: 1, price: 300 },
+      { description: "Hosting (3 months)", quantity: 1, price: 75 },
+      { description: "Domain name (1 year)", quantity: 1, price: 10 }
+    ],
+    tax: 5
+  },
+
+  methods: {
+    addRow() {
+      this.items.push({
+        description: "",
+        quantity: 1,
+        price: 0
+      });
+    }
+  },
+
+  computed: {
+    // calculate sub-total
+    subtotal() {
+      return this.items.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0
+      );
+    },
+    // calculate total
+    total() {
+      return this.items.reduce(
+        (acc, item) =>
+          (acc + item.price * item.quantity) * (1 + this.tax / 100),
+        0
+      );
+    }
+  },
+
+  filters: {
+    currency(value) {
+      return value.toFixed(2);
+    }
+  }
+});
